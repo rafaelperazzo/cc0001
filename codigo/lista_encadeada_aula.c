cd6421a06cece9ee8a22417d8607dcc86e2964f3
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-04-22 15:56:23 -0300

    22/04/2021

--- codigo/lista_encadeada_aula.c
+++ codigo/lista_encadeada_aula.c
@@ -50,11 +50,115 @@ void inserir_inicio(int valor, celula *p) {
     p->prox = nova;
 }
 
+int tamanho(celula *p) {
+    int i=0;
+    while (p->prox!=NULL) {
+        i++;
+        p=p->prox;
+    }
+    return(i);
+}
+
+int vazia(celula *p) {
+    if (p->prox==NULL) {
+        return (1);
+    }
+    else {
+        return (0);
+    }
+}
+
+void inserir_n(int valor, celula *p, int posicao) {
+    int i=0;
+    if (posicao>=tamanho(p)) {
+        printf("Posição inválida!\n");
+        return;
+    }
+    //Chegar na celula anterior
+    for (i=0;i<posicao;i++) {
+        p=p->prox;
+    }
+    //Criar nova celula e ajustar ponteiros
+    celula *nova;
+    nova = nova_celula(valor);
+    nova->prox=p->prox;
+    p->prox = nova;
+}
+
+void remover_inicio(celula *p) {
+
+    if (p->prox!=NULL) {
+        celula *lixo;
+        //O lixo fica sendo o primeiro elemento
+        lixo = p->prox;
+        //O próximo do cabeça fica sendo o próximo do elemento que será removido
+        p->prox = lixo->prox;
+        //Remover a primeira célula
+        free(lixo);
+    }
+}
+
+void remover_final(celula *p) {
+    if (p->prox==NULL) return;
+    //1 - Chegar ao penultimo elemento
+    int i,ultimo;
+    ultimo = tamanho(p)-1;
+    for (i=0;i<ultimo;i++) {
+        p=p->prox;
+    }
+    free(p->prox);
+    p->prox=NULL;
+}
+
+void remover_n(celula *p, int posicao) {
+
+    if (posicao>=tamanho(p)) {
+        printf("Posição inválida!\n");
+        return;
+    }
+    //Chegar a celula anterior a ser removida
+    int i=0;
+    for (i=0;i<posicao;i++) {
+        p=p->prox;
+    }
+    //Ajustar os ponteiros
+    celula *lixo;
+    lixo = p->prox;
+    p->prox=lixo->prox;
+    free(lixo);
+}
+
+void imprimir_como_lista(celula *p) {
+    if (p->prox==NULL) {
+        printf("[INICIO]==>NULL");
+    }
+    else {
+        printf("[INICIO]==>");
+    }
+    while (p->prox!=NULL) {
+        p=p->prox;
+        if (p->prox==NULL) {
+            printf("[%d]==>NULL\n",p->conteudo);
+        }
+        else {
+            printf("[%d]==>",p->conteudo);
+        }
+    }
+}
+
 int main() {
     celula *cabeca;
     cabeca = inicializar();
     inserir_final(10,cabeca);
     inserir_final(20,cabeca);
-    inserir_inicio(100,cabeca);
-    imprimir(cabeca);
+    inserir_final(30,cabeca);
+    inserir_final(40,cabeca);
+    inserir_n(222,cabeca,0);
+    imprimir_como_lista(cabeca);
+    //remover_n(cabeca,2);
+    //imprimir_como_lista(cabeca);
+    remover_final(cabeca);
+    imprimir_como_lista(cabeca);
+    remover_final(cabeca);
+    imprimir_como_lista(cabeca);
 }
